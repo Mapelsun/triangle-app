5fc10eafe86e2790527653d35e25a276c1b83a61
Pels <pelumisunday@gmail.com>
2023-07-26 10:56:03 +0100

    feat: add function to read file

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,11 +1,27 @@
-import { useState, ChangeEvent } from 'react'
+import { useState, ChangeEvent, MouseEvent } from 'react'
 
-export default function FileUploader() {
+type FileUploaderProps = {
+  onFileUpload: (fileContent: string) => void
+}
+
+export default function FileUploader({ onFileUpload }: FileUploaderProps) {
   const [selectedFile, setSelectedFile] = useState<File | null>(null)
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0] ?? null
     setSelectedFile(file)
+  }
+
+  const handleFileUpload = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    if (selectedFile) {
+      const reader = new FileReader()
+      reader.onload = () => {
+        const fileContent = reader.result as string
+        onFileUpload(fileContent)
+      }
+      reader.readAsText(selectedFile)
+    }
   }
 
   return (
@@ -25,7 +41,9 @@ export default function FileUploader() {
           onChange={handleFileChange}
         />
       </label>
-      <button className='round-full bg-violet-50 text-md text-violet-700 py-2 px-4 hover:bg-violet-100 w-full'>
+      <button
+        className='round-full bg-violet-50 text-md text-violet-700 py-2 px-4 hover:bg-violet-100 w-full'
+        onClick={handleFileUpload}>
         Upload File
       </button>
     </form>
