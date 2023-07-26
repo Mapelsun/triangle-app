464a3a94f27706dd324d8eb5a7fac85ae359dc14
Pels <pelumisunday@gmail.com>
2023-07-26 20:28:10 +0100

    refactor: move cell grid logic out of return fn

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -8,6 +8,26 @@ export default function Visualizer({ triangleData }: VisualizerProps) {
   const [maxTotal, setMaxTotal] = useState<number>(0)
   const [highlightedPath, setHighlightedPath] = useState<number[][]>([])
 
+  const cellGrid = triangleData.map((row, rowIndex) => (
+    <div key={rowIndex} className='flex'>
+      {row.map((cell, colIndex) => {
+        const isHighlighted = highlightedPath.some(
+          ([highlightedRow, highlightedCol]) =>
+            highlightedRow === rowIndex && highlightedCol === colIndex
+        )
+        return (
+          <div
+            key={colIndex}
+            className={`w-12 h-12 flex justify-center items-center p-2 m-1 border border-gray-300 rounded ${
+              isHighlighted ? 'bg-violet-300 animate-fade-in' : ''
+            }`}>
+            {cell}
+          </div>
+        )
+      })}
+    </div>
+  ))
+
   useEffect(() => {
     const calculateMaxTotal = () => {
       const dp: number[][] = triangleData.map((row) => [...row])
@@ -47,30 +67,7 @@ export default function Visualizer({ triangleData }: VisualizerProps) {
             <div className='mt-4 flex flex-col items-center'>
               <p>Maximum Total: {maxTotal}</p>
               <div className='mt-8'>
-                <div className='flex flex-col items-center'>
-                  {triangleData.map((row, rowIndex) => (
-                    <div key={rowIndex} className='flex'>
-                      {row.map((cell, colIndex) => {
-                        const isHighlighted = highlightedPath.some(
-                          ([highlightedRow, highlightedCol]) =>
-                            highlightedRow === rowIndex &&
-                            highlightedCol === colIndex
-                        )
-                        return (
-                          <div
-                            key={colIndex}
-                            className={`w-12 h-12 flex justify-center items-center p-2 m-1 border border-gray-300 rounded ${
-                              isHighlighted
-                                ? 'bg-violet-300 animate-fade-in'
-                                : ''
-                            }`}>
-                            {cell}
-                          </div>
-                        )
-                      })}
-                    </div>
-                  ))}
-                </div>
+                <div className='flex flex-col items-center'>{cellGrid}</div>
               </div>
             </div>
           </>
